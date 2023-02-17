d22de730dbe0a36e586863d0af0604d6567e4bae
Vittorio Lo Preiato <vittorio.lopreiato@freedompro.eu>
2023-02-17 17:59:17 +0100

    add gdrive

--- post-install.js
+++ post-install.js
@@ -1,7 +1,39 @@
 `use strict`;
 
-const {
-  execSync
-} = require(`child_process`);
+const fs = require('fs');
 
-execSync(`git clone https://github.com/falconeta/matter-android-libs.git --branch 0.0.1  android/third_party/connectedhomeip/libs`);
+const google = require('@googleapis/drive');
+
+const drive = google.drive('v2');
+
+const decompress = require('decompress');
+
+var dest = fs.createWriteStream("libs.zip");
+drive.files.get(
+  { fileId: '17kCOcpTmRpa-7eE2Ns-qjo-49BTi1ZtH', key: "AIzaSyB9IB-sqbYwCA-FKfo20a0_eJXQOkW8SFw", alt: 'media' },
+  { responseType: "stream" },
+  (err, stream) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    stream.data
+      .on("end", () => {
+        decompress("libs.zip", "android/third_party/connectedhomeip/libs")
+          .then((files) => {
+            fs.unlinkSync('libs.zip')
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+
+        console.log("Done.")
+      })
+      .on("error", (err) => {
+        console.log(err);
+        return process.exit();
+      })
+      .pipe(dest);
+  }
+);
+
